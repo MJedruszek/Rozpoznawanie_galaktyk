b5d51f21f8cffa9b46f10e6a4fee274fb5fc4f51
Małgorzata Jędruszek <275427@student.pwr.edu.pl>
2025-10-06 17:38:30 +0200

    OpenCV tests regarding working with pixels and their values

--- opencv_tests.py
+++ opencv_tests.py
@@ -9,11 +9,12 @@
 import numpy as np
 import cv2 as cv
 import sys
-
-# Otwieranie, wyświetlanie i zapisywanie obrazu
+#######################################################
+# Otwieranie, wyświetlanie i zapisywanie obrazu       #
+#######################################################
 
 # Otwórz plik pod konkretną nazwą/ścieżką
-img = cv.imread("images_gz2/images/10000.jpg")
+img = cv.imread("images_gz2/images/8500.jpg")
 
 # Czy plik istnieje?
 if img is None:
@@ -26,3 +27,56 @@ k = cv.waitKey(0)
 # Zapisz, jeśli użytkownik kliknął "s"
 if k == ord("s"):
     cv.imwrite("results/galaxy.png", img)
+
+
+#######################################################
+# Zmiana wartości pikseli, właściwości obrazu         #
+#######################################################
+
+# pobierz i wyświetl wartości BGR danego piksela
+# format [ B G R]
+# dla czarnobiałych wychodzi tylko jasność piksela
+
+px = img[100,100]
+print( px )
+
+# pobierz i wyświetl intensywność niebieskiego piksela
+
+blue = img[100,100,0]
+print(blue)
+
+# # zmień wartość piksela na zadaną
+# img[100,100] = [255,255,255]
+
+# cv.imshow("Display window", img)
+# k = cv.waitKey(0)
+ 
+# # Zapisz, jeśli użytkownik kliknął "s"
+# if k == ord("s"):
+#     cv.imwrite("results/galaxy.png", img)
+
+# ^^^ tak warto tylko jeden zmienić, do większych operacji raczej nie
+
+# odbierz i pokaż wymiary obrazka (x,y, liczba kanałów)
+
+#print(img.shape)
+
+# skopiuj i wklej jakiś obszar w konkretne miejsce
+
+piece = img[180:240, 180:240]
+img[100:160, 100:160] = piece
+
+# pobierz konkretny kolor
+# r = img[:,:,2]
+
+# podziel obraz na trzy oddzielne kanały (wolniejsze od powyższego)
+b,g,r = cv.split(img)
+
+cv.imshow("Display window", r)
+k = cv.waitKey(0)
+
+cv.imshow("Display window", g)
+k = cv.waitKey(0)
+
+cv.imshow("Display window", b)
+k = cv.waitKey(0)
