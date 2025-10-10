65a88a8327c4ac6ab97ba77cde2bc9b5f6233bbd
Małgorzata Jędruszek <275427@student.pwr.edu.pl>
2025-10-10 19:14:21 +0200

    Added HED edge detection

--- main.py
+++ main.py
@@ -6,48 +6,54 @@ from matplotlib import pyplot as plt
 #Krok 0: wczytaj obraz w skali szarości
 
 filename = "images_gz2/images/8500.jpg"
-img = cv.imread(filename, cv.IMREAD_GRAYSCALE)
+img_gray = cv.imread(filename, cv.IMREAD_GRAYSCALE)
+#ten sam, ale kolorowy dla heda
+img_color = cv.imread(filename)
 
 #Krok 1: Normalizacja
-norm = cv.normalize(img, None, 0, 255, cv.NORM_MINMAX)
+# norm = cv.normalize(img, None, 0, 255, cv.NORM_MINMAX)
 
 #Krok 2: Filtracja (póki co filtr bilateral o losowych parametrach XD)
 
-filtered = cv.bilateralFilter(norm,7,75,75)
+# filtered = cv.bilateralFilter(norm,7,75,75)
 
-#Krok 3: Progowanie
+# #Krok 3: Progowanie
 
-ret, thresh = cv.threshold(filtered, 16, 256, cv.THRESH_BINARY)
+# ret, thresh = cv.threshold(filtered, 16, 256, cv.THRESH_BINARY)
 
 #i druga opcja, progowanie adaptacyjne
-thresh = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
-            cv.THRESH_BINARY,3,2)
+# thresh = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
+#             cv.THRESH_BINARY,3,2)
 
 #Krok 3.5: Wyświetl kolejne kroki od 0 do 3 w celu sprawdzenia poprawności wykonania (opcjonalne)
 
-titles = ['Original Image', 'After normalization',
-            'After filtering', 'After thresholding']
-images = [img, norm, filtered, thresh]
+# titles = ['Original Image', 'After normalization',
+#             'After filtering', 'After thresholding']
+# images = [img, norm, filtered, thresh]
 
-for i in range(4):
-    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
-    plt.title(titles[i])
-    plt.xticks([]),plt.yticks([])
-plt.show()
+# for i in range(4):
+#     plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+#     plt.title(titles[i])
+#     plt.xticks([]),plt.yticks([])
+# plt.show()
 
 #Krok 4: Detekcja krawędzi 
 
 # Głupi Canny
-edges_canny = cv.Canny(img, 20,100)
-
-cv.imshow("Canny",edges_canny)
-k = cv.waitKey(0)
+edges_canny = cv.Canny(img_gray, 20,100)
 
 #HED
-W,H = 424
-blob = cv.dnn.blobFromImage(img, scalefactor=1.0, size=(W, H),swapRB=False, crop=False)
+W = 424
+H = 424
+blob = cv.dnn.blobFromImage(img_color, scalefactor=1.0, size=(W, H),swapRB=False, crop=False)
 
-#JUTRO:
-#POBIERZ POTRZEBNE PLIKI Z NETA
-#PRZETESTUJ WSZYSTKIE TRZY DETEKTORY, KTÓRE POWINNAŚ
-net = cv.dnn.readNetFromCaffe("path to prototxt file", "path to model weights file")
+net = cv.dnn.readNetFromCaffe("HED/deploy.prototxt", "HED/hed_pretrained_bsds.caffemodel")
+
+net.setInput(blob)
+hed = net.forward()
+hed = cv.resize(hed[0, 0], (W, H))
+hed = (255 * hed).astype("uint8")
+cv.imshow("Input", img_gray)
+cv.imshow("HED", hed)
+cv.imshow("Canny", edges_canny)
+cv.waitKey(0)
